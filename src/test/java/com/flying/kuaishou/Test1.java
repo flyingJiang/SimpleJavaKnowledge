3840d61b15a58e7da5ecc3243c08226ca94e5246
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-08-09 21:49:05 +0800

    2020-08-09 注释掉编译不通过代码

--- src/test/java/com/flying/kuaishou/Test1.java
+++ src/test/java/com/flying/kuaishou/Test1.java
@@ -9,7 +9,7 @@ public class Test1 {
     public void m1(){
         List[]  p = new List[10];
         //泛型
-        List<String>[] p1 = new List<String>[10];
+//        List<String>[] p1 = new List<String>[10];
         //3.某文件多列，tab隔开，统计第三列单词，按照单词出现的次数，做倒序展示，并同时展示次数
         //4.
         //@ControllerAdvice 是否可以捕获 servlet 里面 filter的异常
