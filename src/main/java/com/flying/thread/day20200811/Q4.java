4982a0d8557d9db44b5acde13e1f7bacfb34bced
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-08-11 11:33:37 +0800

    2020-08-11 多线程

---
+++ src/main/java/com/flying/thread/day20200811/Q4.java
@@ -0,0 +1,7 @@
+package com.flying.thread.day20200811;
+/**
+ * 第四：线程和纤程的区别的是什么？为什么纤程比较轻量级？
+ *
+ */
+public class Q4 {
+}
---
+++ src/main/java/com/flying/thread/day20200811/Q6.java
@@ -0,0 +1,15 @@
+package com.flying.thread.day20200811;
+/**
+ * 第六：下列三种业务，应该如何使用线程池：
+ * 高并发、任务执行时间短
+ * 并发不高、任务执行时间长
+ * 并发高、业务执行时间长
+ *
+ * http://ifeve.com/%E5%A6%82%E4%BD%95%E4%BC%98%E9%9B%85%E7%9A%84%E4%BD%BF%E7%94%A8%E5%92%8C%E7%90%86%E8%A7%A3%E7%BA%BF%E7%A8%8B%E6%B1%A0/
+ *
+ * https://blog.csdn.net/weixin_33743880/article/details/91871577
+ *
+ *
+ */
+public class Q6 {
+}
