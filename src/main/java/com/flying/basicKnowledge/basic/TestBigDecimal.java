90dfffcd6fec229b69db9269144b2de3790264b1
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-08-20 14:13:17 +0800

    2020-08-20 测试

--- src/main/java/com/flying/basicKnowledge/basic/TestBigDecimal.java
+++ src/main/java/com/flying/basicKnowledge/basic/TestBigDecimal.java
@@ -4,17 +4,21 @@ import java.math.BigDecimal;
 
 public class TestBigDecimal {
     public static void main(String[] args) {
-        BigDecimal a = BigDecimal.valueOf(0.0333);
-        System.out.println(a);
+//        BigDecimal a = BigDecimal.valueOf(0.0333);
+//        System.out.println(a);
+//
+//        System.out.println(a.intValue());
+//        if (a.equals(BigDecimal.ZERO)){
+//
+//        }
+//        BigDecimal b = null;
+//        if (b.equals(BigDecimal.ZERO)){
+//
+//        }
+        int a =10;
+        long b = 10;
+        System.out.println(a==b);
 
-        System.out.println(a.intValue());
-        if (a.equals(BigDecimal.ZERO)){
-
-        }
-        BigDecimal b = null;
-        if (b.equals(BigDecimal.ZERO)){
-
-        }
 
     }
 }
