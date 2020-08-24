9b4952ee0dc75d76a45712334690aff54e297b6b
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-08-24 16:47:50 +0800

    2020-08-24 SpringBoot 三招组合拳，手把手教你打出优雅的后端接口

--- src/main/java/com/flying/basicKnowledge/basic/Test.java
+++ src/main/java/com/flying/basicKnowledge/basic/Test.java
@@ -1,8 +1,28 @@
 package com.flying.basicKnowledge.basic;
 
+import java.util.Collections;
+
 public class Test {
+    //    public static void main(String[] args) {
+//        String str = "Hello ";
+//        str += "world";
+//    }
+//    static void increment(int index) {
+//        index++;
+//    }
+//
+//    public static void main(String[] args) {
+//        int i = 0;
+//        increment(i);
+//        i++;
+//        System.out.println(i);
+////        ArraySet
+//        Collections.emptyIterator();
+//    }
     public static void main(String[] args) {
-        String str = "Hello ";
-        str += "world";
+        double a = 3.14;
+        //Error:(24, 18) java: incompatible types: double cannot be converted to java.lang.Float
+//        Float b =a;
     }
+
 }
--- src/main/java/com/flying/basicKnowledge/basic/TestBigDecimal.java
+++ src/main/java/com/flying/basicKnowledge/basic/TestBigDecimal.java
@@ -3,7 +3,7 @@ package com.flying.basicKnowledge.basic;
 import java.math.BigDecimal;
 
 public class TestBigDecimal {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 //        BigDecimal a = BigDecimal.valueOf(0.0333);
 //        System.out.println(a);
 //
@@ -15,10 +15,27 @@ public class TestBigDecimal {
 //        if (b.equals(BigDecimal.ZERO)){
 //
 //        }
-        int a =10;
-        long b = 10;
-        System.out.println(a==b);
+//        int a =10;
+//        long b = 10;
+//        System.out.println(a==b);
 
 
+    //    }
+    public static void main(String[] args) {
+        Integer a = new Integer(3);
+        Integer b = 3;  // 将3自动装箱成Integer类型
+        int c = 3;
+        System.out.println(a == b); // false 两个引用没有引用同一对象
+        System.out.println(a.equals(b)); // true
+        System.out.println(a == c); // true a自动拆箱成int类型再和c比较
+        System.out.println(b == c); // true
+
+        Integer a1 = 128;
+        Integer b1 = 128;
+        System.out.println(a1 == b1); // false
+
+        Integer a2 = 127;
+        Integer b2 = 127;
+        System.out.println(a2 == b2); // true
     }
 }
